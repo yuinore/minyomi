a656a057a5d734534c846ac0ba53cbc0849207d5
yuinore <yuinore@hotmail.co.jp>
2023-02-25 15:58:00 +0900

    Update db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# !!!!DANGER!!!!
+Choice.destroy_all
+Word.destroy_all
+
+word_linux = Word.create!(name: "Linux", slug: "linux", tags: "#Linux #OS")
+word_constexpr = Word.create!(name: "constexpr", slug: "constexpr", tags: "#C言語 #C++")
+
+Choice.create!(name: "リナックス", count: 10, auth_count: 3, word: word_linux)
+Choice.create!(name: "リヌックス", count: 1, auth_count: 2, word: word_linux)
+
+Choice.create!(name: "コンストイーエクスピーアール", count: 21, auth_count: 8, word: word_constexpr)
+Choice.create!(name: "コンストエクスプレ", count: 9, auth_count: 7, word: word_constexpr)
+Choice.create!(name: "コンストエクスプル", count: 1, auth_count: 0, word: word_constexpr)
