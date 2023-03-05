5cae511da380547db6b9571794eeb604fdf3d88e
yuinore <yuinore@hotmail.co.jp>
2023-03-06 05:26:58 +0900

    user の null 許可

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,4 @@
 class Vote < ApplicationRecord
   belongs_to :choice
-  belongs_to :user
+  belongs_to :user, optional: true
 end
--- db/migrate/20230305201310_create_votes.rb
+++ db/migrate/20230305201310_create_votes.rb
@@ -2,8 +2,8 @@ class CreateVotes < ActiveRecord::Migration[7.0]
   def change
     create_table :votes do |t|
       t.references :choice, null: false, foreign_key: true
-      t.boolean :authenticated
-      t.references :user, null: false, foreign_key: true
+      t.boolean :authenticated, null: false
+      t.references :user, foreign_key: true
       t.text :session
 
       t.timestamps
