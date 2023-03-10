5d0c4a40ff4980c1ef506724e8e2f5bcf71ef2ac
yuinore <yuinore@hotmail.co.jp>
2023-03-10 17:32:30 +0900

    bundle exec rubocop -A

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -9,16 +9,16 @@ class ChoicesController < ApplicationController
     word = Word.find(params[:word_id])
 
     # TODO: 403を返す
-    return "403" if word.slug != params[:word_slug]
+    return '403' if word.slug != params[:word_slug]
 
     # TODO: ひらがなのみで構成されているかチェック（カタカナでも良いけど平仮名のほうが好きかも）
     # TODO: 重複チェック
 
     Choice.create!(
-      word: word,
+      word:,
       name: params[:new_choice],
       count: 0,
-      auth_count: 0,
+      auth_count: 0
     )
 
     # TODO: Vote.create!
