52bb94387e9317cc1474e4d60eea10cc4d00feb7
yuinore <yuinore@hotmail.co.jp>
2023-03-10 16:55:42 +0900

    Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_24_215104) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_25_071754) do
+  create_table "choices", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
+    t.text "name"
+    t.integer "count"
+    t.integer "auth_count"
+    t.bigint "word_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["created_at"], name: "index_choices_on_created_at"
+    t.index ["updated_at"], name: "index_choices_on_updated_at"
+    t.index ["word_id"], name: "index_choices_on_word_id"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["created_at"], name: "index_users_on_created_at"
+    t.index ["updated_at"], name: "index_users_on_updated_at"
   end
 
+  create_table "words", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
+    t.text "name"
+    t.string "slug"
+    t.text "tags"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["created_at"], name: "index_words_on_created_at"
+    t.index ["slug"], name: "index_words_on_slug", unique: true
+    t.index ["updated_at"], name: "index_words_on_updated_at"
+  end
+
+  add_foreign_key "choices", "words"
 end
